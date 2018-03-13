8edaafd51ccb7beda8deff11ddfc379de50a9549
feebleblobber <bennystapleton@gmail.com>
2018-03-13 18:58:43 -0400

    Update customer.py

--- customer.py
+++ customer.py
@@ -1,21 +1,43 @@
 class Customer(Person):
   __accountList = []
-  __accountCount = 0
+  accountNum = 1
 
-  def __init__(self, name, age, eid, cashOnHand):
+  def __init__(self, name, age, eid):
     Person.__init__(name, age, eid)
-    self.cashOnHand = cashOnHand
+  
+  def getAccountList(self):
+  # function name: getAccountList
+  #
+  # parameters: none
+  # description of function: This function returns the values within the encapsulated variable accountList
+    return self.accountList
   
   def makeAccount(self, initBalance, fee = 0, accnt_min = 0):
+  # function name: makeAccount
+  #
+  # parameters: initBalance - initial balance for the account, fee - ATM fee amount, 
+  #   accnt_min - minimum required balance for the account
+  # description of function: This function creates a new Account object 
+    
+    # Checks to ensure that only someone old enough to hold an account is creating the account
     if self.age < 16:
       print("You are too young to create an account!")
       return
     
     accountList.append(Account.__init__(initBalance, self.eid, str(self.eid) + "." + str(accountCount), fee, accnt_min))
+    accountCount +=1
     
-  	def request_loan(self, amount):
-		  self.debt = amount
-
-	  def pay_loan(self, amount):
-		  self.debt -= amount
-    
+  def deposit(self, amount, account):
+  # function name: deposit
+  #
+  # parameters: amount - the amount being deposited (a float), account - the index of the account in the customer's list of accounts
+  # description of function: This function deposits some money into the account being referenced.
+    accountList[account].makeDeposit(amount, self.eid)
+  
+  def withdraw(self, amount, account):
+  # function name: withdraw
+  #
+  # parameters: amount - the amount being deposited, account - the index of the account in the customer's list of accounts
+  # description of function: This funtion withdraws some amount of money from the account at index account.
+  #   This is done by calling the 
+  
