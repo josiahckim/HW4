5e8490ef8c59a468bb03391404f98c86d12328c2
feebleblobber <bennystapleton@gmail.com>
2018-03-13 18:19:35 -0400

    Create customer.py

---
+++ customer.py
@@ -0,0 +1,15 @@
+class Customer(Person):
+  __accountList = []
+  __accountCount = 0
+
+  def __init__(self, name, age, eid, cashOnHand):
+    Person.__init__(name, age, eid)
+    self.cashOnHand = cashOnHand
+  
+  def makeAccount(self, initBalance, fee = 0, accnt_min = 0):
+    if self.age < 16:
+      print("You are too young to create an account!")
+      return
+    
+    accountList.append(Account.__init__(initBalance, self.eid, str(self.eid) + "." + str(accountCount), fee, accnt_min))
+    
