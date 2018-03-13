ff6e39a552d5042549c433cb63edc62237ed0fe4
feebleblobber <bennystapleton@gmail.com>
2018-03-13 17:58:57 -0400

    Update account.py

--- account.py
+++ account.py
@@ -1,4 +1,8 @@
 class Account:
+  __balance = 0
+  __accountID = 0
+  __holderID = 0
+  
   def __init__(self, balance, holderID, accountID, fees, account_min = 0):
     self.balance = balance
     self.holderID = holderID
@@ -15,7 +19,10 @@ class Account:
   def makeDeposit(self, amount):
     self.balance += amount
   
-  def makeWithdrawal(self, amount, isATM):
+  def makeWithdrawal(self, amount, isATM, personID):
+    # checks the account holder against the accessor's ID
+    if not(checkOwner(personID)):
+      return
     if isATM:
       # checking if the amount and ATM fee will reduce the account below its minimum
       if self.balance - amount - self.fees < self.account_min:
@@ -27,3 +34,7 @@ class Account:
       if self.balance - amount < self.account_min:
         print("insufficient funds")
       self.balance -= amount
+
+  def checkOwner(self, personID):
+    # checks if the person attempting to access the account is the account holder
+    return self.holderID == personID
