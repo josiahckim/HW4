0f02e408ae2e57db9dd785bb9b349d6d10e19863
josiahckim <36700309+josiahckim@users.noreply.github.com>
2018-03-13 12:35:07 -0400

    Add files via upload

---
+++ Assistant Class.py
@@ -0,0 +1,22 @@
+from Person Class.py import Person
+from Account.py import Account
+
+class Manager(Person):
+    def __init__(self, name, age, eid):
+    	Person.__init__(self, name, age)
+    	self.eid = eid  
+
+    def isEmployee(self):
+        return True  
+
+    def view_balance(self, account):
+    	Person.view_balance(self, account):
+
+    def make_account(self, amount, account_name):
+    	Person.make_account(self, amount, account_name)
+
+    def get_name(self):
+    	Person.get_name(self)
+
+### Add specific functions only accesible by an Assistant (EMCAPSULATE)
+
