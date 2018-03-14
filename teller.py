bf3af2e4ad2e76d1595b216baf964a44709f7f2d
josiahckim <36700309+josiahckim@users.noreply.github.com>
2018-03-13 21:58:20 -0400

    Update teller.py

--- teller.py
+++ teller.py
@@ -1,19 +1,11 @@
-from Person Class.py import Person
-from Account.py import Account
-
 class Teller(Person):
     def __init__(self, name, age, eid):
     	Person.__init__(self, name, age)
-    	self.eid = eid  
-
-    def isEmployee(self):
-        return True  
+    	self.eid = eid
 
     def view_balance(self, account):
     	Person.view_balance(self, account):
 
     def get_name(self):
     	Person.get_name(self)
-
-### Add specific functions only accesible by an Teller (EMCAPSULATE)
 
