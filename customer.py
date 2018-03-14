0ebcf0debb85781b9ea78fde4e6f3616af792c6d
feebleblobber <bennystapleton@gmail.com>
2018-03-13 21:03:26 -0400

    Update customer.py

--- customer.py
+++ customer.py
@@ -1,3 +1,6 @@
+from account.py import class Account
+from person.py import class Person
+
 class Customer(Person):
   __accountList = []
   accountNum = 1
@@ -24,25 +27,37 @@ class Customer(Person):
       print("You are too young to create an account!")
       return
     
-    accountList.append(Account.__init__(initBalance, self.eid, str(self.eid) + "." + str(accountCount), fee, accnt_min))
-    accountCount +=1
+    accountList.append(Account.__init__(initBalance, self.eid, str(self.eid) + "." + str(accountCount), self.name, fee, accnt_min))
+    accountCount += 1
     
   def deposit(self, amount, account):
   # function name: deposit
   #
   # parameters: amount - the amount being deposited (a float), account - the index of the account in the customer's list of accounts
-  # description of function: This function deposits some money into the account being referenced.
+  # description of function: This funtion deposits some amount of money into the account at index account.
+  #   This is done by calling the makeDeposit function for the referenced account.
     accountList[account].makeDeposit(amount, self.eid)
   
-  def withdraw(self, amount, account):
+  def withdraw(self, amount, ATM, account):
   # function name: withdraw
   #
-  # parameters: amount - the amount being deposited, account - the index of the account in the customer's list of accounts
+  # parameters: amount - the amount being deposited, account - the index of the account in the customer's list of accounts, 
+  #   ATM - a boolean variable that is true if the transaction is occuring at an ATM.
   # description of function: This funtion withdraws some amount of money from the account at index account.
-  #   This is done by calling the 
+  #   This is done by calling the makeWithdrawal function for the referenced account.
+    accountList[account].makeWithdrawal(amount, ATM, self.eid)
+  
+  def viewAccounts(self):
+  # function name: viewAccounts
+  #
+  # paramters: none
+  # description of function: This function displays all of the accounts the person currently holds
+    for account in accountList:
+      print(account)
+      
 
-  	def request_loan(self, amount):
-      self.debt = amount
+  def request_loan(self, amount):
+    self.debt = amount
 
-    def pay_loan(self, amount):
-      self.debt -= amount
+  def pay_loan(self, amount):
+    self.debt -= amount
